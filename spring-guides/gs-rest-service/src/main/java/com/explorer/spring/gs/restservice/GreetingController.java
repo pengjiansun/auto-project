1f8fa9567f4d58a95dc2a98791c16441ef5137c8
sunpengjian <pengjiansun@163.com>
2022-03-31 14:43:18 +0800

    w

--- spring-guides/gs-rest-service/src/main/java/com/explorer/spring/gs/restservice/GreetingController.java
+++ spring-guides/gs-rest-service/src/main/java/com/explorer/spring/gs/restservice/GreetingController.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    private static final String template = "hello, %s!";
+    private static final String template = "hello,w %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
